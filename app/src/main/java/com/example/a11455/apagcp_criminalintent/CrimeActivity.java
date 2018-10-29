af85d275a620db4081e2dea53051704c1d1974de
shencang <2630610009@qq.com>
2018-10-29 21:17:14 +0800

    2018.10.29:请看日志

--- app/src/main/java/com/example/a11455/apagcp_criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/a11455/apagcp_criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.example.a11455.apagcp_criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,5 +17,23 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+
+        /**
+         * 代码清单7-15 获取FragmentManager
+         */
+        FragmentManager fm = getSupportFragmentManager();
+
+        /**
+         * 代码清单7-16 添加一个CrimeFragment
+         */
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment==null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
     }
 }
