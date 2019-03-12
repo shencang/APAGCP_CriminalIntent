dbc828282b7ba2f3f523ba5ddbd3ca2d34892efb
shencang <2630610009@qq.com>
2019-03-12 22:54:15 +0800

    第八章

--- app/src/main/java/com/example/a11455/apagcp_criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/a11455/apagcp_criminalintent/CrimeListActivity.java
@@ -1,4 +1,17 @@
 package com.example.a11455.apagcp_criminalintent;
 
-public class CrimeListActivity {
+import android.support.v4.app.Fragment;
+
+/*
+代码清单 8-9 清理CrimeActivity类-3
+ */
+
+/*
+代码清单 8-10 实现CrimeListActivity
+ */
+public class CrimeListActivity  extends SingleFragmentActivity{
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeFragment();
+    }
 }
--- app/src/main/java/com/example/a11455/apagcp_criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/a11455/apagcp_criminalintent/CrimeListFragment.java
@@ -1,4 +1,15 @@
 package com.example.a11455.apagcp_criminalintent;
 
-public class CrimeListFragment {
+    /*
+    代码清单 8-9 清理CrimeActivity类-3
+     */
+
+import android.support.v4.app.Fragment;
+
+/*
+代码清单 8-11 实现CrimeListFragment
+ */
+public class CrimeListFragment  extends Fragment {
+
+    //Nothing yet
 }
