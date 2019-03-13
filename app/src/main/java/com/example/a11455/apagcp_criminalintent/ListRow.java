b5cabe0771a86f9699ebb31023a5d4d9a042fa3c
shencang <2630610009@qq.com>
2019-03-13 22:57:06 +0800

    第八章

---
+++ app/src/main/java/com/example/a11455/apagcp_criminalintent/ListRow.java
@@ -0,0 +1,24 @@
+package com.example.a11455.apagcp_criminalintent;
+
+
+/*
+   代码清单8-13 典型的ViewHolder
+ */
+//
+//import android.view.View;
+//import android.widget.ImageView;
+//
+//import java.util.List;
+//
+//public class ListRow extends RecyclerView.ViewHolder{
+//    public ImageView mThumbnail;
+//
+//    public ListRow(View view){
+//        super(view);
+//    }
+//    mThumbnail=(ImageView)view.findViewById(R.id.thumbnail);
+
+//    ListRow row =new ListRow(inflater.inflate(R.layout.list_row,parent,false));
+//    View view = row.itemView;
+//    ImageView thumbnailView = row.mThumbnail;
+//}
