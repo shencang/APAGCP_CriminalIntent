cfd8f5618b954fb10c2c11ac2877e95a5fdea84d
shencang <2630610009@qq.com>
2019-05-01 21:51:01 +0800

    第十二章节部分内容

--- app/src/main/java/com/example/a11455/apagcp_criminalintent/Fragment/DatePickerFragment.java
+++ app/src/main/java/com/example/a11455/apagcp_criminalintent/Fragment/DatePickerFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.example.a11455.apagcp_criminalintent.R;
 
@@ -18,9 +20,19 @@ public class DatePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        /*
+        代码清单 12-4 给AlterDialog添加DatePicker-1
+         */
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
         //return super.onCreateDialog(savedInstanceState);
+
+        /*
+        代码清单 12-4 给AlterDialog添加DatePicker-2
+         */
         return new AlertDialog
                 .Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
